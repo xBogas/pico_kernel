1b6ee40b61b1cdf39bc5fb50078b4a7d45217ab3
xBogas <119903183+xBogas@users.noreply.github.com>
2023-11-19 03:49:58 +0000

    include/init: separate implementation in src file

--- include/init.h
+++ include/init.h
@@ -12,24 +12,12 @@
 #include "hardware/structs/scb.h"
 #include "hardware/regs/resets.h"
 
-extern int main();
-
 static int cpuid() {
 	return sio_hw->cpuid;
 }
 
-static void main_wrapper(void)
-{
-	main();
-}
-
-static void kernel_entry(void) {
-	stdio_init_all();
-	while (!stdio_usb_connected())
-		;
-	multicore_launch_core1(main_wrapper);
-	printf("exit kernel_entry\n");
-}
+// this cannot be constructor
+void /* __attribute__((constructor))  */ kernel_entry(void);
 
 extern char __StackLimit; /* Set by linker.  */
 
