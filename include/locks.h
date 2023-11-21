b70a7d225256dd2a8c1abcf135930a91238c0d66
xBogas <119903183+xBogas@users.noreply.github.com>
2023-11-21 18:23:13 +0000

    include/locks: add struct spinlock

---
+++ include/locks.h
@@ -0,0 +1,41 @@
+#ifndef W_KERNEL_LOCKS_H
+#define W_KERNEL_LOCKS_H
+
+#include "pico.h"
+
+// Spinlock
+struct spinlock
+{
+	uint32_t *lock;
+	uint32_t irq;
+
+//#ifdef DEBUG_KERNEL
+	char* name;
+	uint32_t cpu;
+//#endif
+};
+
+/**
+ * @brief Initialize a spinlock
+ * 
+ * @param lock object to initialize
+ * @param name must not be a temporary variable
+ */
+void init_lock(struct spinlock *lock, const char *name);
+
+/**
+ * @brief Acquire a spinlock
+ * spins until lock is acquired
+ * 
+ * @param lock 
+ */
+void acquire_lock(struct spinlock *lock);
+
+/**
+ * @brief Release a spinlock
+ * 
+ * @param lock 
+ */
+void release_lock(struct spinlock *lock);
+
+#endif
