825c46d99f976cac038cc48b64c1f840a32f8904
xBogas <119903183+xBogas@users.noreply.github.com>
2023-11-21 18:23:37 +0000

    src/spinlock: add implementation for spinlock

---
+++ src/spinlock.c
@@ -0,0 +1,22 @@
+#include "locks.h"
+#include "init.h"
+#include <stdio.h>
+
+void init_lock(struct spinlock *lock, const char *name) {
+	
+	lock->lock = spin_lock_init(spin_lock_claim_unused(1));
+	lock->irq = 0;
+	lock->name = name;
+	lock->cpu = -1;
+}
+
+void acquire_lock(struct spinlock *lock) {
+	lock->irq = spin_lock_blocking(lock->lock);
+	lock->cpu = get_core_num();
+}
+
+void release_lock(struct spinlock *lock) {
+	spin_unlock(lock->lock, lock->irq);
+	lock->irq = 0;
+	lock->cpu = -1;
+}
