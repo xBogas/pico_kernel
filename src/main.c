de079b489de771afaec36b188c06124612df6fce
xBogas <119903183+xBogas@users.noreply.github.com>
2023-11-27 03:46:17 +0000

    src/main: force hardfault in mpu setup

--- src/main.c
+++ src/main.c
@@ -1,22 +1,27 @@
 #include "stdio.h"
 #include "memory.h"
 #include "init.h"
+#include "hardware/irq.h"
+#include "terminal.h"
+#include "tusb.h"
 
 int main(void)
 {
 	if (cpuid() == 0){
-		printf("core 0 entered main\n");
+		printk("core 0 entered main\n");
 		k_mem_init();
-		
-		//start_sched();
-		sleep_ms(5000);
+
+		uint32_t start = time_us_32();
+		while (time_us_32() - start < 1000000) {
+			printk("hello\n");
+			sleep_ms(10);
+		}
 	}
 	else {
-		printf("core 1 entered main\n");
-		while (1){
-			sleep_ms(3000);
-			printf("core 1 running\n");
-		}
+		printk("core 1 running ...\n");
+		while (1)
+			usb_task();
+
 	}
 
 	return 0;
