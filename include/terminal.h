aacacb9554fb89542d380b11fc71f68fd4897cff
xBogas <119903183+xBogas@users.noreply.github.com>
2023-11-29 04:18:45 +0000

    include/terminal: Separate usb exclusive functions Consolidate header dependencies

--- include/terminal.h
+++ include/terminal.h
@@ -1,9 +1,15 @@
 #ifndef W_KERNEL_TERMINAL_H_INCLUDED
 #define W_KERNEL_TERMINAL_H_INCLUDED
 
-int term_connected(void);
+#include <stdarg.h>
+
 void term_init(void);
-void term_flush(void);
-void printk(const char *str);
+void printk(const char *str, ...);
+void vprintk(const char *fmt, va_list ap);
+
+#if KERNEL_CONSOLE_USB
+int term_connected(void);
 void usb_task(void);
+#endif
+
 #endif
