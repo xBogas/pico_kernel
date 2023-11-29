aa661a503aaa8c6725addeb02bd3fbc2f0bfb920
xBogas <119903183+xBogas@users.noreply.github.com>
2023-11-29 04:46:10 +0000

    include/init: remove headers

--- include/init.h
+++ include/init.h
@@ -1,16 +1,7 @@
 #ifndef W_KERNEL_INIT_H
 #define W_KERNEL_INIT_H
 
-#include <stdint.h>
-#include "pico/stdio.h"
-#include "pico/multicore.h"
-#include "hardware/resets.h"
-#include "hardware/clocks.h"
-
-// hardware registers
 #include "hardware/structs/sio.h"
-#include "hardware/structs/scb.h"
-#include "hardware/regs/resets.h"
 
 static inline int cpuid() {
 	return sio_hw->cpuid;
