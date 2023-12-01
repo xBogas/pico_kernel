818905a5036e05cf5d69b5e173f3dce83fa1bdd1
xBogas <119903183+xBogas@users.noreply.github.com>
2023-12-01 17:33:46 +0000

    include/memory: add mpu functions

--- include/memory.h
+++ include/memory.h
@@ -6,10 +6,12 @@
 
 void k_mem_init(void);
 
-void* k_malloc(void);
+void *k_malloc(void);
 
 void k_free(void* ptr);
 
+void *new_mpu_zone(uint8_t access, uint32_t size);
 
+void *expand_mpu_zone(uint8_t id, uint32_t add_len);
 
 #endif
