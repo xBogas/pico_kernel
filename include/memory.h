42bd87facbc7ffba31934953404cb79ab161cafb
xBogas <119903183+xBogas@users.noreply.github.com>
2023-12-05 01:42:45 +0000

    include/memory: add condition for mpu usage. Export kernel definitions

--- include/memory.h
+++ include/memory.h
@@ -1,17 +1,35 @@
 #ifndef W_KERNEL_MEMORY_H
 #define W_KERNEL_MEMORY_H
 
-#include <stdlib.h>
-#include <stdint.h>
+#include "kernel.h"
+
+#ifdef KERNEL_USE_MPU
+#define MPU_AP_NO_ACCESS 0x00	// No access
+#define MPU_AP_PRIV 0x01		// Privileged access only
+#define MPU_AP_U_RO 0x02		// User read-only
+#define MPU_AP_FULL_ACCESS 0x03 // Full access
+// #define MPU_AP_RESERVED		0x04 // Reserved
+#define MPU_AP_PRIV_RO 0x05 // Privileged read-only
+#define MPU_AP_R0 0x06		// Privileged and user read-only
+#endif
+
+#define PAGE_SIZE 			1024 // 0x400
+
+#define PG_ROUND_UP(x) 		((x + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1))
+#define PG_ROUND_DOWN(x)	((x) & ~(PAGE_SIZE - 1))
+
+#define N_PAGES(x)			PG_ROUND_UP(x) / PAGE_SIZE
+
 
 void k_mem_init(void);
-
 void *k_malloc(void);
-
-void k_free(void* ptr);
-
+void k_free(void *ptr);
 void *new_mpu_zone(uint8_t access, uint32_t size);
-
 void *expand_mpu_zone(uint8_t id, uint32_t add_len);
 
+// These are supposed to be user functions
+void *malloc(size_t size);
+void *calloc(size_t count, size_t size);
+void *realloc(void *ptr, size_t size);
+void free(void *ptr);
 #endif
