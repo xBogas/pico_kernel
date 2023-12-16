3e21b3a339b625ad7218d428dc773e3ba1f08d49
xBogas <119903183+xBogas@users.noreply.github.com>
2023-12-16 01:48:29 +0000

    include/thread: Remove unused macro. Change struct thread_handle so there is no offset for stack_ptr. Add busy wait method for threads.

--- include/thread.h
+++ include/thread.h
@@ -4,7 +4,6 @@
 #include "memory.h"
 
 #define MIN_STACK_SIZE 			PAGE_SIZE
-#define OFFSET_THREAD_ENTRY		20u
 
 enum th_state {
 	Ready,
@@ -28,21 +27,22 @@ struct thread_attr {
 	uint16_t	stack_size;
 };
 
-//! Changing stack_ptr off will break context switch
-//! Export stack_ptr offset
+//! Set stack ptr to first 4 bytes
 struct thread_handle {
+	uint32_t stack_ptr;
 	uint16_t id;
 	uint16_t priority;
 	const char *name;
 	uint16_t state;
 	uint16_t stack_size; // not needed
-	uint32_t stack_ptr;
 	uint32_t stack_top;	 // size is this addr to its value
 };
 
 int thread_create(void (*thread_fn)(void *data),
 					void *data,
 					struct thread_attr *attr);
+
+void wait(uint32_t ms);
 
 //TODO:
 
