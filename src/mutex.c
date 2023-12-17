eb483eeae541cecc96be34de87a9adc04393bb3d
xBogas <119903183+xBogas@users.noreply.github.com>
2023-12-17 18:02:49 +0000

    src/mutex: Implementation of mutex

---
+++ src/mutex.c
@@ -0,0 +1,33 @@
+#include "locks.h"
+#include "scheduler.h"
+
+void init_mutex(struct mutex *mtx, const char *name)
+{
+	printk("initializing mutex %s\n", name);
+	init_lock(&mtx->sp_lk, name);
+	mtx->owner = MUTEX_UNOWNED;
+}
+
+void acquire_mutex(struct mutex *mtx)
+{
+	if (!mtx)
+		panic("acquire_mutex: mutex is NULL\n");
+
+	acquire_lock(&mtx->sp_lk);
+	while (mtx->lock)
+		sleep(mtx);
+
+	mtx->lock = 1;
+	mtx->owner = mythread()->id;
+	release_lock(&mtx->sp_lk);
+}
+
+
+void release_mutex(struct mutex *mtx)
+{
+	acquire_lock(&mtx->sp_lk);
+	mtx->lock = 0;
+	mtx->owner = MUTEX_UNOWNED;
+	wake_up(mtx);
+	release_lock(&mtx->sp_lk);
+}
