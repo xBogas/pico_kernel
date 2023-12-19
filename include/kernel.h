e724d78cbeaf20e21eee46f9c8104eef0cd2f96a
xBogas <119903183+xBogas@users.noreply.github.com>
2023-12-19 23:06:43 +0000

    include/kernel: Include cmsis library. Add privileged mode check.

--- include/kernel.h
+++ include/kernel.h
@@ -7,11 +7,18 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stddef.h>
+
 #include "pico/platform.h"
+#include "RP2040.h"
 
 void __attribute__((noreturn)) panic(const char *fmt, ...);
 
 void __attribute__((noreturn)) exit(int status);
+
+static inline uint32_t is_privileged(void)
+{
+	return (__get_current_exception()) || !(__get_CONTROL() & 1);
+}
 
 #define cpu_id()		get_core_num()
 
